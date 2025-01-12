ef0c5797d3851f0bebf9a987f1d8a7b0396b3ef2
mpedych <maksym.pedych@gmail.com>
2025-01-12 18:38:25 +0200

    Initial commit

---
+++ src/main/java/com/pedych/naimar/NaimarApplication.java
@@ -0,0 +1,13 @@
+package com.pedych.naimar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaimarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaimarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedych/naimar/NaimarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedych.naimar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaimarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
